c7a54e8f3886dc711b8779c291de04ef3393a884
yskkwc <y.k.trial.serv@gmail.com>
2020-12-09 15:19:40 +0900

    Change devise controllers_with_templetes_for_loginform & redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!, except: [:home]
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    users_path
+  end
+
+  def after_sign_up_path_for(resource)
+    users_path
+  end
+
+  def after_update_path_for(resource)
+    users_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 end
