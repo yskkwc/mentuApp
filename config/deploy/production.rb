2d469344f7ce6908b01a7f6f3396fa4f0d9ee3a7
yskkwc <y.k.trial.serv@gmail.com>
2021-01-19 19:08:01 +0900

    Revert deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '172.31.45.142', user: 'ysk', roles: %w(app db web)
+server '18.179.227.152', user: 'ysk', roles: %w(app db web)
 ##
 # role-based syntax
 # ==================
